c88d4b38cc056eb61d510986efe8082cc84b18bd
CCQL <cquinonez@opc.com.co>
2020-11-13 21:15:14 -0500

    Init Repo

---
+++ OdeToFood.Web/Controllers/GreetingController.cs
@@ -0,0 +1,23 @@
+﻿using OdeToFood.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class GreetingController : Controller
+    {
+        // GET: Greeting
+        public ActionResult Index()
+        {
+            var model = new GreetingViewModel()
+            {
+                Message = ConfigurationManager.AppSettings["message"]
+            };
+            return View(model);
+        }
+    }
+}
---
+++ OdeToFood.Web/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using OdeToFood.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        IRestaurantData db;
+
+        public HomeController(IRestaurantData db)
+        {
+            this.db = db; 
+        }
+        public ActionResult Index()
+        {
+            var model = db.GetAll();
+            return View(model);
+            //return "<h1>Hello world</h1>";  
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
